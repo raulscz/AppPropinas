85f791c4f5baa7a45cb05872bb7362dfdf65b9b4
Raul <100007530.joan23@fje.edu>
2021-10-11 17:14:13 +0200

    Actualización Error JS

--- js/code.js
+++ js/code.js
@@ -1,8 +1,8 @@
 function calculo() {
     //alert("Hellow") 
-    let totalCuenta = docuement.getElementById('cuenta').value;
-    let servicio = docuement.getElementById('servicio').value;
-    let personas = docuement.getElementById('personas').value;
+    let totalCuenta = document.getElementById('cuenta').value;
+    let servicio = document.getElementById('servicio').value;
+    let personas = document.getElementById('personas').value;
 
     console.log('total: ' + totalCuenta + ' servicio: ' + servicio + ' personas: ' + personas)
 }
